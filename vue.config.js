c3425d547d32cb810bc6d8f5aa62e9123c687482
wangchunsheng <chunsheng.wang@ys100.com>
2020-08-24 11:28:15 +0800

    增加打包自动压缩功能

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 // Vue.config.js 配置选项
-
+const FileManagerPlugin = require('filemanager-webpack-plugin')
 module.exports = {
 
     // 选项
@@ -21,6 +21,22 @@ module.exports = {
                 return args
             })
     },
-
+    configureWebpack: { //webpack的相关配置在这里
+        plugins: [
+            new FileManagerPlugin({ //初始化 filemanager-webpack-plugin 插件实例
+                onEnd: {
+                    delete: [ //首先需要删除项目根目录下的dist.zip
+                        './dist.zip',
+                    ],
+                    archive: [ //然后我们选择dist文件夹将之打包成dist.zip并放在根目录
+                        {
+                            source: './dist',
+                            destination: './dist.zip'
+                        },
+                    ]
+                }
+            })
+        ]
+    }
 
 }
